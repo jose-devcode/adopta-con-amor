e9130311279c144e5e723125d4a08fc93bb475b8
unknown <dxt.nis@gmail.com>
2025-06-11 21:28:55 -0300

    Estructura base

---
+++ src/app/perro/[id]/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <main className='p-4'>
+      <h1 className='text-3xl font-bold'>Adoptá con Amor</h1>
+      <p className='mt-2'>
+        Explorá perritos en adopción y encontrá un nuevo compañero.
+      </p>
+    </main>
+  )
+}
